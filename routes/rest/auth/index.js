c3b0040390a048b52acf8fb37dcff4153c94acf1
Saumya Som <saumyasom92@gmail.com>
2023-05-15 00:49:26 +0530

    Security Inital

--- routes/rest/auth/index.js
+++ routes/rest/auth/index.js
@@ -1,15 +1,23 @@
+const jwt = require("jsonwebtoken")
 const User = require("../../../models/user")
 
 module.exports = {
-  async psot(req, res) {
+  async post(req, res) {
     try {
       const { email, password } = req.body
 
       const user = await User.findOne({ email, password }).exec()
-
       if (!user) {
         return res.render("login")
       }
+
+      const payload = {
+        _id: user._id,
+        email: user.email
+      }
+      const token = jwt.sign(payload, process.env.SECRET, {
+        expiresIn: 3600 * 24 * 1
+      })
 
       return res.redirect("/")
     } catch (error) {
--- routes/rest/index.js
+++ routes/rest/index.js
@@ -1,11 +1,22 @@
 const express = require("express")
 const router = express.Router()
 
+const { expressjwt: jwt } = require("express-jwt")
+const checkJwt = jwt({ secret: process.env.SECRET, algorithms: ["HS256"] })
+
 const auth = require("./auth")
 const url = require("./urls")
 const user = require("./users")
 
-router.post("/user/login", auth.psot)
+router.post("/user/login", auth.post)
+
+// router.all("*", checkJwt, (err, req, res, next) => {
+//   if (err.name === "UnauthorizedError") {
+//     res.status(401).send("Invalid Token")
+//   } else {
+//     next()
+//   }
+// })
 
 router.post("/create", url.post)
 router.get("/:id", url.get)
