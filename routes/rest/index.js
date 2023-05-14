15a20192907e773fdbdffb338b7fcfc4df09f6c9
Saumya Som <saumyasom92@gmail.com>
2023-05-14 21:19:49 +0530

    Analytics Api Added

--- routes/rest/index.js
+++ routes/rest/index.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const url = require("./urls")
 
 router.post("/create", url.post)
+router.get("/sorturl/:id", url.get)
+router.get("/analytics/:id", url.analytics)
 
 module.exports = router
--- routes/rest/urls.js
+++ routes/rest/urls.js
@@ -12,7 +12,7 @@ module.exports = {
       const shortId = shortid.generate(7)
       console.log(shortId.verbose)
 
-      const url = await URL.create({
+      await URL.create({
         sortId: shortId,
         redirectUrl: originalUrl
       })
@@ -25,5 +25,49 @@ module.exports = {
         .status(500)
         .json({ error: true, reason: error.message })
     }
+  },
+
+  // eslint-disable-next-line consistent-return
+  async get(req, res) {
+    try {
+      const { id } = req.params
+      if (typeof id === "undefined") return res.status(400).json({ error: true, reason: "Sort Id is Required" })
+
+      const entry = await URL.findOneAndUpdate(
+        { sortId: id },
+        {
+          $push: {
+            visitHistory: {
+              timeStamp: Date.now()
+            }
+          }
+        }
+      )
+      res.redirect(entry.redirectUrl)
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: true, reason: error.message })
+    }
+  },
+
+  async analytics(req, res) {
+    try {
+      const { id } = req.params
+      if (typeof id === "undefined") return res.status(400).json({ error: true, reason: "Sort Id is Required" })
+
+      const url = await URL.findOne({ sortId: id }).exec()
+
+      const count = url.visitHistory.length
+
+      return res
+        .status(200)
+        .json({ error: false, data: count })
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: true, reason: error.message })
+    }
   }
+
 }
